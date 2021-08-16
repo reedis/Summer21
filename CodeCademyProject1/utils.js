85fcdcd482d66fd6105db61c673fc133328a052d
Isaac R <ireed574@gmail.com>
2021-08-16 15:16:21 -0400

    Added Quote Project

--- CodeCademyProject1/utils.js
+++
@@ -1,8 +0,0 @@
-const getRandomElement = arr => {
-  if (!Array.isArray(arr)) throw new Error('Expected an array');
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-module.exports = {
-  getRandomElement
-};
