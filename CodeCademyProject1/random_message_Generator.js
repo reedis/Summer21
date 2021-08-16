eb03e0445dac84346486889e8e1b24e2254e9b29
Isaac R <ireed574@gmail.com>
2021-08-16 15:20:06 -0400

    removed the hat game

--- CodeCademyProject1/random_message_Generator.js
+++
@@ -1,7 +0,0 @@
-function main() {
-    let ranMessage = Math.floor(Math.random() * 10);
-    let messages = ["Do some streching", "Call someone!", "Take a 15 minute break", "Look away from the screen for a second!", "Go for a walk!",
-                    "Make your bed!", "Drink some Water", "Is there a task you are forgetting", "Make sure to finish the checkList today!", "Today will be a good day!"];
-        console.log(messages[ranMessage]);
-}
-main();
--- random_message_Generator.js
+++ random_message_Generator.js
@@ -1,40 +1,7 @@
 function main() {
     let ranMessage = Math.floor(Math.random() * 10);
-    switch(ranMessage) {
-        case 0:
-            console.log("Today will be a good day!");
-            break;
-        case 1:
-            console.log("Make sure to finish the checkList today!");
-            break;
-        case 2:
-            console.log("Is there a task you are forgetting");
-            break;
-        case 3:
-            console.log("Drink some Water");
-            break;
-        case 4:
-            console.log("Make your bed!");
-            break;
-        case 5:
-            console.log("Go for a walk!");
-            break;
-        case 6:
-            console.log("Look away from the screen for a second!");
-            break;
-        case 7:
-            console.log("Take a 15 minute break");
-            break;
-        case 8:
-            console.log("Call someone!");
-            break;
-        case 9:
-            console.log("Do some streching");
-            break;
-        default:
-            console.log("I dont know how you got here!");
-            break;
-    }
+    let messages = ["Do some streching", "Call someone!", "Take a 15 minute break", "Look away from the screen for a second!", "Go for a walk!",
+                    "Make your bed!", "Drink some Water", "Is there a task you are forgetting", "Make sure to finish the checkList today!", "Today will be a good day!"];
+        console.log(messages[ranMessage]);
 }
-
 main();
